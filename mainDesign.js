92d9dd47d2ac061052ba09d75d76bec502f7ecc0
Nikita Kozhukhov <knik0405@gmail.com>
2023-09-16 11:20:57 +0300

    v0.1.1. Добавил общий стиль для страницы

---
+++ mainDesign.js
@@ -0,0 +1,5 @@
+let html = document.querySelector('html');
+html.style.cssText = "font-size: 62.5%; overflow-x: hidden; margin: 0; padding: 0; box-sizing: border-box; text-decoration: none; border: none; outline: none; scroll-behavior: smooth; font-family: Kreadon, Arial, sans-serif;"
+
+let body = document.querySelector('body');
+body.style.cssText = "background-color:  #48416a; color: #fff; "
