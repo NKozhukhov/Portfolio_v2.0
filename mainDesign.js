aa93fe2fbea81b6790780ae12cf1a3e567b01e3f
Nikita Kozhukhov <knik0405@gmail.com>
2023-09-16 11:41:15 +0300

    Добавление шрифтов

--- mainDesign.js
+++ mainDesign.js
@@ -3,3 +3,13 @@ html.style.cssText = "font-size: 62.5%; overflow-x: hidden; margin: 0; padding: 
 
 let body = document.querySelector('body');
 body.style.cssText = "background-color:  #48416a; color: #fff; "
+
+
+/* Шрифты */
+
+let head = document.querySelector('head');
+head.insertAdjacentHTML('beforeend', '<style> @font-face {font-family: "Kreadon"; font-style: normal; font-weight: normal; src: local("Kreadon"), url("/fonts/regular_Kreadon.ttf") format("ttf");} @font-face {font-family: "Kreadon"; font-style: normal; font-weight: lighter; src: local("Kreadon"), url("/fonts/light_Kreadon.ttf") format("ttf");} @font-face {font-family: "Kreadon"; font-style: normal; font-weight: bold; src: local("Kreadon"), url("/fonts/bold_Kreadon.ttf") format("ttf");} </style>')
+
+
+
+
