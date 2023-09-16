cf05d1e1f4a90303af0706b84568e6c17018eb4e
Nikita Kozhukhov <knik0405@gmail.com>
2023-09-16 22:31:29 +0300

    привязка цветовых стилей

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let html = document.querySelector('html');
 html.style.cssText = "font-size: 62.5%; overflow-x: hidden; margin: 0; padding: 0; scroll-behavior: smooth; font-family: Kreadon, Arial, sans-serif;"
 
 let body = document.querySelector('body');
-body.style.cssText = "background-color:  #48416a; color: #fff; "
+body.style.cssText = "background-color: var(--bg-color); color: #fff; "
 
 
 /* Шрифты */
@@ -12,9 +12,16 @@ body.style.cssText = "background-color:  #48416a; color: #fff; "
 let head = document.querySelector('head');
 head.insertAdjacentHTML('beforeend', '<style> @font-face {font-family: "Kreadon"; font-style: normal; font-weight: normal; src: local("Kreadon"), url("/fonts/regular_Kreadon.ttf") format("ttf");} @font-face {font-family: "Kreadon"; font-style: normal; font-weight: lighter; src: local("Kreadon"), url("/fonts/light_Kreadon.ttf") format("ttf");} @font-face {font-family: "Kreadon"; font-style: normal; font-weight: bold; src: local("Kreadon"), url("/fonts/bold_Kreadon.ttf") format("ttf");} </style>')
 
+/* цветовые стили */
 
-
-
+document.documentElement.style.setProperty('--bg-color', '#48416a');
+document.documentElement.style.setProperty('--text-color', '#fff');
+document.documentElement.style.setProperty('--second-bg-color', '#60578b');
+document.documentElement.style.setProperty('--main-color', '#ebc08d');
+document.documentElement.style.setProperty('--blue-color', '#9dbfed');
+document.documentElement.style.setProperty('--violet-color', '#b79af4');
+document.documentElement.style.setProperty('--peach-color', '#eeab9b');
+document.documentElement.style.setProperty('--dark-color', '#383258');
 
 
 
@@ -23,13 +30,13 @@ head.insertAdjacentHTML('beforeend', '<style> @font-face {font-family: "Kreadon"
 let header = document.createElement('header');
 header.className = 'header';
 header.id = 'header';
-header.style.cssText = "box-sizing: border-box; position: fixed; top: 0; left: 0; width: 100%; padding: 1rem 5%; background-color: #48416a; display: flex; justify-content: space-between; align-items: center; z-index: 20;"
+header.style.cssText = "box-sizing: border-box; position: fixed; top: 0; left: 0; width: 100%; padding: 1rem 5%; background-color: var(--bg-color); display: flex; justify-content: space-between; align-items: center; z-index: 20;"
 body.insertAdjacentElement('afterbegin', header);
 
 let logo = document.createElement('img');
 logo.className = 'logo';
 logo.src = 'images/Logo.png'
-logo.style.cssText = "width: 8rem; height: auto; filter: drop-shadow(0 0 1rem #383258)";
+logo.style.cssText = "width: 8rem; height: auto; filter: drop-shadow(0 0 1rem var(--dark-color))";
 header.insertAdjacentElement('afterbegin', logo);
 
 let nav = document.createElement('nav');
