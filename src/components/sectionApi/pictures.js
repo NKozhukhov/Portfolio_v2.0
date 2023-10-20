29277bf38d6b17da63f8df37faeeb3bb51c3d057
Nikita Kozhukhov <knik0405@gmail.com>
2023-10-20 04:15:06 +0300

    подключение API картинки

--- src/components/sectionApi/pictures.js
+++ src/components/sectionApi/pictures.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import "../../style/main.sass"
+import axios from 'axios'
+
+const basicUrl = "https://dog.ceo/api/breeds/image/random"
 
 class Pictures extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            message: ["https://images.dog.ceo/breeds/hound-ibizan/n02091244_5400.jpg"],
+    }
+    }   
+
+    
     render(){
         return (
+
+            
             <div className="pictures-container">
                 <h3>Pictures</h3>
-                <button>Следующая</button>
+                <button onClick={() => {axios.get(basicUrl).then((res) => {
+                    this.setState({message: res.data.message})
+                })}}>
+                    Следующая</button>
                 <div className='pictures-box'>
-                    <img></img>
+                    <img src={this.state.message}></img>
                 </div>
             </div>
         )
-    }
+
 }
+}
+
+
 
 export default Pictures 
